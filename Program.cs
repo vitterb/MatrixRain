6f477b300f5793ee92347e890711beb9127ffb67
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2023-02-24 19:07:07 +0000

    Added comments to code

--- Program.cs
+++ Program.cs
@@ -7,18 +7,25 @@ namespace Matrix
 
         static void Main (string[] args)
         {
+            // setting start and end point in ascii table to get printable symbols
             int AsciiStart = 62;
             int AsciiEnd = 400;
+            //setting up counters to use for changes in colors and display positions
             int counter = 0;
             int counter2 = 0;
+            //setting up variables to use for axis
             int x = 0;
             int y = 0;
+            // creating a random generator
             Random rnd = new Random();
+            // making the cursor invisable
             Console.CursorVisible = false;
-
+            // starting infinite loop
             while (true)
-            {         
+            {   
+                // getting a random number from the ascii table
                 int num = rnd.Next(AsciiStart, AsciiEnd);
+                // setting cursor position
                 if (counter2 == 0)
                 {
                     y = rnd.Next(0, Console.WindowHeight -11); ;
@@ -30,7 +37,7 @@ namespace Matrix
                     Console.SetCursorPosition(x, y++);
                 }
 
-
+                // randomizing colours of the symbols
                 if (counter % 5 == 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -52,13 +59,15 @@ namespace Matrix
                     Console.ResetColor();
                     System.Threading.Thread.Sleep(10);
                 }
-                
+                // updating counters
                 counter++;
                 counter2++;
-                if (counter2 == rnd.Next(5,9) || counter2 == 10)
+                // using counter2 to make lines between 5 and 9 symbols long and adding a saftey net 
+                if (counter2 == rnd.Next(5,9) || counter2 >= 10)
                 {
                     counter2 = 0;
                 } 
+                // When 5000 loops have been completed Clear console.
                 if (counter > 5000)
                 {
                     Console.Clear();
